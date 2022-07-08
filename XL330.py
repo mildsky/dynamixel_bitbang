7fabef35c051ffe9bf63796a0e940ad32bdf47cc
Mat0imaru <hubo.move@gmail.com>
2022-07-08 08:55:41 +0100

    optimization for delay when send packet

--- XL330.py
+++ XL330.py
@@ -32,9 +32,9 @@ class xl330:
             packet)
         wid = self.io.wave_create()
         self.io.wave_send_once(wid)
-        time.sleep(0.05)
+        time.sleep(0.00075*packet_len)
         if not self.InPin == None:
-            count, data = self.io.bb_serial_read(self.InPin)
+            _, data = self.io.bb_serial_read(self.InPin)
             # print(' '.join('%02x'%b for b in data[packet_len:]))
             return data[packet_len:]
         return None
@@ -80,7 +80,7 @@ def main():
     motor.ping()
     motor.write(ADDR_LED, SIZE_LED, 1)
     motor.write(ADDR_TORQ_ENA, SIZE_TORQ_ENA, 1)
-    T = 20
+    T = 200
     for i in range(T+1):
         motor.write(ADDR_GOAL_POS, SIZE_GOAL_POS, 2048+1024*math.sin(2*i/T*math.pi))
         time.sleep(0.01)
