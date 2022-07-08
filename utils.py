280304a426aaa040ac3e34e7faace9f4394f1feb
Mat0imaru <hubo.move@gmail.com>
2022-07-08 05:55:13 +0100

    util func: N bytearray to int

--- utils.py
+++ utils.py
@@ -1,7 +1,13 @@
-def toNBytes(number, length):
+def IntToNBytes(number, length):
     ret = []
+    number = int(number)
     for i in range(length):
         ret.append(number & 0xff)
         number = number >> 8
     return ret
-    
+
+def nBytesToInt(bytes, length):
+    ret = 0
+    for i in range(length):
+        ret += bytes[i] << (8*i)
+    return ret
